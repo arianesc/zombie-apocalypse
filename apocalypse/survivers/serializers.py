ad3378a2a16c7285092a518587c9f693f76a74a0
arianesc <arianesilveira97@hotmail.com>
2019-07-06 22:59:16 -0300

    Refactor serializers.py

--- apocalypse/survivers/serializers.py
+++ apocalypse/survivers/serializers.py
@@ -2,10 +2,13 @@ from rest_framework import serializers
 from survivers.models import Surviver
 
 
-class SurviverSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(required=True, allow_blank=False, max_length=100)
-    age = serializers.PositiveIntegerField(required=True, allow_blank=False)
-    gender = serializers.CharField(required=True, allow_blank=False, max_length=10)
-    latitude = serializers.CharField(required=True, allow_blank=False, max_length=100)
-    longitude = serializers.CharField(required=True, allow_blank=False, max_length=100)
+class SurviverSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Surviver
+        fields = ('id', 'name', 'age', 'gender', 'latitude', 'longitude')
+
+    def create(self, validated_data):
+        """
+        Create and return a new `Surviver` instance
+        """
+        return Surviver.objects.create(**validated_data)
