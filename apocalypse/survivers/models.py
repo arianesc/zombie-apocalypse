be7c90b51b9c3d272c098ff7ebff6420740593f0
arianesc <arianesilveira97@hotmail.com>
2019-07-06 22:06:06 -0300

    Create model to survivers

--- apocalypse/survivers/models.py
+++ apocalypse/survivers/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
+class Surviver(models.Model):
+    name = models.CharField(max_length=100, blank=True, default='')
+    age = models.PositiveIntegerField()
+    gender = models.CharField( max_length = 10)
+    latitude = models.CharField(max_length = 100, blank=True, null=True)
+    longitude = models.CharField(max_length = 100, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 # Create your models here.
