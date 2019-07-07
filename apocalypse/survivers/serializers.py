1792582c92a67cfdaf22bf0523c946f59e023fc0
arianesc <arianesilveira97@hotmail.com>
2019-07-07 15:30:27 -0300

    modify serializer

--- apocalypse/survivers/serializers.py
+++ apocalypse/survivers/serializers.py
@@ -5,7 +5,7 @@ from survivers.models import Surviver
 class SurviverSerializer(serializers.ModelSerializer):
     class Meta:
         model = Surviver
-        fields = ('id', 'name', 'age', 'gender', 'latitude', 'longitude')
+        fields = ('id', 'name', 'age', 'gender', 'latitude', 'longitude', 'food', 'water', 'medication', 'ammunition', 'infected')
 
     def create(self, validated_data):
         """
