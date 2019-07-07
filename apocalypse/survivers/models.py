e57adb2cbc72eaeb0cddbbf5c2234d88992044a6
arianesc <arianesilveira97@hotmail.com>
2019-07-07 15:29:49 -0300

    Add new fields on models

--- apocalypse/survivers/models.py
+++ apocalypse/survivers/models.py
@@ -4,11 +4,15 @@ from django.core.validators import MinValueValidator
 
 class Surviver(models.Model):
     name = models.CharField(max_length=100, blank=True, default='')
-    age = models.IntegerField(validators=[MinValueValidator(1)])
+    age = models.IntegerField(validators=[MinValueValidator(0)])
     gender = models.CharField( max_length=10)
     latitude = models.CharField(max_length=100, blank=True, null=True)
     longitude = models.CharField(max_length=100, blank=True, null=True)
+    food = models.IntegerField(validators=[MinValueValidator(0)], default=0)
+    water = models.IntegerField(validators=[MinValueValidator(0)], default=0)
+    medication = models.IntegerField(validators=[MinValueValidator(0)], default=0)
+    ammunition = models.IntegerField(validators=[MinValueValidator(0)], default=0)
+    infected = models.IntegerField(validators=[MinValueValidator(0)], default=0)
 
     def __str__(self):
         return self.name
-# Create your models here.
