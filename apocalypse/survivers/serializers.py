897f2533f38a5cfe81367a6d93a6c50a102ad97a
arianesc <arianesilveira97@hotmail.com>
2019-07-07 20:21:28 -0300

    Refactor serializers.py

--- apocalypse/survivers/serializers.py
+++ apocalypse/survivers/serializers.py
@@ -5,10 +5,5 @@ from survivers.models import Surviver
 class SurviverSerializer(serializers.ModelSerializer):
     class Meta:
         model = Surviver
-        fields = ('id', 'name', 'age', 'gender', 'latitude', 'longitude', 'food', 'water', 'medication', 'ammunition', 'infected')
-
-    def create(self, validated_data):
-        """
-        Create and return a new `Surviver` instance
-        """
-        return Surviver.objects.create(**validated_data)
+        fields = ('id', 'name', 'age', 'gender', 'latitude', 'longitude',
+                  'food', 'water', 'medication', 'ammunition', 'infected')
