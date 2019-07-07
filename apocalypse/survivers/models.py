7de05aacb26bdadc28cb6151606a09fac942d799
arianesc <arianesilveira97@hotmail.com>
2019-07-06 22:57:30 -0300

    use ModelSerializer

--- apocalypse/survivers/models.py
+++ apocalypse/survivers/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 
 class Surviver(models.Model):
     name = models.CharField(max_length=100, blank=True, default='')
-    age = models.PositiveIntegerField()
-    gender = models.CharField( max_length = 10)
-    latitude = models.CharField(max_length = 100, blank=True, null=True)
-    longitude = models.CharField(max_length = 100, blank=True, null=True)
+    age = models.IntegerField(validators=[MinValueValidator(1)])
+    gender = models.CharField( max_length=10)
+    latitude = models.CharField(max_length=100, blank=True, null=True)
+    longitude = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.name
