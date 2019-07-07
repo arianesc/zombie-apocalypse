a34bd4ed5742891e66e9813f8b1648b6f7244e49
arianesc <arianesilveira97@hotmail.com>
2019-07-07 20:21:47 -0300

    Add new routes

--- apocalypse/survivers/urls.py
+++ apocalypse/survivers/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('api/survivers/create', views.survivers_create),
     path('api/survivers/update_location/<int:pk>/', views.surviver_update_location),
     path('api/survivers/relate_infected/<int:pk>/', views.relate_infected),
+    path('api/survivers/statistics', views.percentage_infected_survivors),
+    path('api/survivers/report', views.show_reports),
 
 ]
