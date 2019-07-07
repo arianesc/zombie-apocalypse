81dff69584feed9f448d3b68a344700567bca3f5
arianesc <arianesilveira97@hotmail.com>
2019-07-07 15:33:26 -0300

    Separate create, list, update location in different methods

--- apocalypse/survivers/views.py
+++ apocalypse/survivers/views.py
@@ -1,50 +1,70 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from survivers.models import Surviver
 from survivers.serializers import SurviverSerializer
+from rest_framework.response import Response
 
-@csrf_exempt
+ITEMS = {'water': 4 , 'food': 3 , 'medications': 2 , 'ammunition': 1 }
+
+@api_view(['POST'])
+def survivers_create(request):
+    """
+    create a new surviver.
+    """
+    if request.method == 'POST':
+        serializer = SurviverSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
 def survivers_list(request):
     """
-    List all code survivers, or create a new surviver.
+    list all survivers.
     """
     if request.method == 'GET':
         survivers = Surviver.objects.all()
         serializer = SurviverSerializer(survivers, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = SurviverSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
-
-@csrf_exempt
-def surviver_detail(request, pk):
+@api_view(['GET', 'PUT'])
+def surviver_update_location(request, pk):
     """
-    Retrieve, update or delete a code surviver.
+    Retrieve or update a code surviver.
     """
     try:
         survivers = Surviver.objects.get(pk=pk)
     except Surviver.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         survivers = SurviverSerializer(survivers)
-        return JsonResponse(survivers.data)
+        return Response(survivers.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = SurviverSerializer(survivers, data=data)
+        serializer = SurviverSerializer(survivers, data=request.data, partial=True)
+        for value in request.data.keys():
+            print(value)
+            if value != "latitude" and value != "longitude":
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
-        survivers.delete()
-        return HttpResponse(status=204)
+@api_view(['PUT'])
+def relate_infected(request, pk):
+    """
+    Relate infected surviver
+    """
+
+    pass
+
+
+
