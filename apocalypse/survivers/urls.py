22e87199d66fb7506d75276bdf2dcfacc727a56b
arianesc <arianesilveira97@hotmail.com>
2019-07-07 16:13:27 -0300

    Add route to relate infected surviver

--- apocalypse/survivers/urls.py
+++ apocalypse/survivers/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('api/survivers/', views.survivers_list),
     path('api/survivers/create', views.survivers_create),
     path('api/survivers/update_location/<int:pk>/', views.surviver_update_location),
+    path('api/survivers/relate_infected/<int:pk>/', views.relate_infected),
 
 ]
