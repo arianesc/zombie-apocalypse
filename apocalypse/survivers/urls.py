007cd5a8308eeba22a1cd5dca9918f40a0f237b5
arianesc <arianesilveira97@hotmail.com>
2019-07-08 22:00:36 -0300

    add new route to trades

--- apocalypse/survivers/urls.py
+++ apocalypse/survivers/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('api/survivers/create', views.survivers_create),
     path('api/survivers/update_location/<int:pk>/', views.surviver_update_location),
     path('api/survivers/relate_infected/<int:pk>/', views.relate_infected),
-    path('api/survivers/statistics', views.percentage_infected_survivors),
     path('api/survivers/report', views.show_reports),
+    path('api/survivers/trades_item/<int:pk1>/<int:pk2>/', views.trades_item_surviver),
 
 ]
